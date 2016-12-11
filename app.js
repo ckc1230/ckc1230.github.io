8b337b59de21dd510e64add32a307e5b79f38966
ckc1230 <chriskchan1230@gmail.com>
2016-12-10 19:15:26 -0800

    projects displayed in carousel

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   console.log( "ready!" );
   $('.parallax').parallax();
+  $('#resume-btn').click(function() {
+    $('#resume-modal').modal('open');
+  })
+  $('.modal').modal();
   $('.carousel.carousel-slider').carousel({full_width: true});
-  // $('.slider').slider({full_width: true, autoplay: false});
-
   $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
@@ -23,26 +25,34 @@ $(document).ready(function() {
     $('#project-carousel').carousel('set', 0);
     $('#project-description-carousel').carousel('set', 0);
     $('#launch-project-btn').attr("href", "https://young-harbor-53388.herokuapp.com/")
+    $('#launch-project-btn').css("border", "2px solid white")
   })
   $('#project-menu-item-1').click(function() {
     $('#project-carousel').carousel('set', 1);
     $('#project-description-carousel').carousel('set', 1);
     $('#launch-project-btn').attr("href", "https://pawsadopt.herokuapp.com/")
-
+    $('#launch-project-btn').css("border", "2px solid green")
   })
   $('#project-menu-item-2').click(function() {
     $('#project-carousel').carousel('set', 2);
     $('#project-description-carousel').carousel('set', 2);
     $('#launch-project-btn').attr("href", "https://trekkster.herokuapp.com/")
+    $('#launch-project-btn').css("border", "2px solid white")
   })
   $('#project-menu-item-3').click(function() {
     $('#project-carousel').carousel('set', 3);
     $('#project-description-carousel').carousel('set', 3);
     $('#launch-project-btn').attr("href", "http://www.storkstore.us/")
+    $('#launch-project-btn').css("border", "2px solid lightblue")
   })
   $('#project-menu-item-4').click(function() {
     $('#project-carousel').carousel('set', 4);
     $('#project-description-carousel').carousel('set', 4);
     $('#launch-project-btn').attr("href", "https://ckc1230.github.io/blackbeards-revenge/")
+    $('#launch-project-btn').css("border", "2px solid red")
   })
+  $(".project-menu-div").click(function() {
+        $(".project-menu-div").removeClass('project-menu-div-active');
+        $(this).addClass('project-menu-div-active');
+    });      
 });
